0ad563c1825cd78cc75558ed122ca2c99b4e0f09
Mindy McAdams <mmcadams@gmail.com>
2018-02-24 19:53:19 -0500

    add mysql

---
+++ web_scraping/more-from-mitchell/mysql_db_example.py
@@ -0,0 +1,42 @@
+# first install PyMySQL with - pip install PyMySQL
+# start XAMPP and start all services
+# see https://github.com/PyMySQL/PyMySQL
+
+import pymysql.cursors
+
+# connect to the local database
+conn = pymysql.connect(host='localhost',
+                    port=8080,
+                    unix_socket='/tmp/mysql.sock',
+                    user='root',
+                    password=None,
+                    db='myshoutbox',
+                    charset='utf8',
+                    cursorclass=pymysql.cursors.DictCursor)
+
+# variables for new record - used below
+name = "Monty"
+shout = "Python rules!"
+mydate = "12:12:12 pm 02-24-2018"
+
+try:
+    with conn.cursor() as cursor:
+        # create a new record
+        sql = "INSERT INTO `shouts` (`name`, `shout`, `date`) VALUES (%s, %s, %s)"
+        # format strings in Python: string (%s), integer (%d),
+        # float (%f), and float restricted to 2 decimal places (%.2f)
+        cursor.execute(sql, (name, shout, mydate))
+
+    # the connection is not autocommit by default - you must commit to
+    # save your changes
+    conn.commit()
+
+    with conn.cursor() as cursor:
+        # read a single record
+        sql = "SELECT `name`, `shout` FROM `shouts` WHERE `date`=%s"
+        cursor.execute(sql, ('01:26:00 pm 01-22-2018',))
+        result = cursor.fetchone()
+        print(result)
+
+finally:
+    conn.close()
