1a4f1b232cf78354280cbccf8480215087028bb8
Mindy McAdams <mmcadams@gmail.com>
2018-02-25 08:16:38 -0500

    update PyMySQL script
    fixed - does not work with XAMPP-VM

--- web_scraping/more-from-mitchell/mysql_db_example.py
+++ web_scraping/more-from-mitchell/mysql_db_example.py
@@ -1,13 +1,12 @@
 # first install PyMySQL with - pip install PyMySQL
-# start XAMPP and start all services
+# start XAMPP and start all services - NOTE: not XAMPP-VM
+# this does not work with XAMPP-VM, bit does work with plain XAMPP
 # see https://github.com/PyMySQL/PyMySQL
 
 import pymysql.cursors
 
 # connect to the local database
 conn = pymysql.connect(host='localhost',
-                    port=8080,
-                    unix_socket='/tmp/mysql.sock',
                     user='root',
                     password=None,
                     db='myshoutbox',
@@ -21,7 +20,7 @@ mydate = "12:12:12 pm 02-24-2018"
 
 try:
     with conn.cursor() as cursor:
-        # create a new record
+        # create a new record in the database
         sql = "INSERT INTO `shouts` (`name`, `shout`, `date`) VALUES (%s, %s, %s)"
         # format strings in Python: string (%s), integer (%d),
         # float (%f), and float restricted to 2 decimal places (%.2f)
@@ -32,7 +31,8 @@ try:
     conn.commit()
 
     with conn.cursor() as cursor:
-        # read a single record
+        # read a single record that exists in the database
+        # note, this is a record in MY local databse, not yours 
         sql = "SELECT `name`, `shout` FROM `shouts` WHERE `date`=%s"
         cursor.execute(sql, ('01:26:00 pm 01-22-2018',))
         result = cursor.fetchone()
