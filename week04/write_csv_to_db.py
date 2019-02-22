1187d3cc2466693f9436dd60a6e4e0121ad76955
macloo <mmcadams@gmail.com>
2019-02-22 13:53:25 -0500

    add file
    This file adapted from my Books Hopper project, CS50_2019

---
+++ week04/write_csv_to_db.py
@@ -0,0 +1,30 @@
+"""
+See https://www.sqlalchemy.org/ - SQLAlchemy must be installed in your virtualenv.
+I used a PostgreSQL db on Heroku.
+Your empty database table must already exist.
+Table fieldnames must be set.
+The environment variable "DATABASE_URL" must be set on the computer this runs on.
+"""
+
+import csv
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine(os.getenv("DATABASE_URL")) # environment variable
+db = scoped_session(sessionmaker(bind=engine))
+
+def main():
+    f = open("books.csv")
+    reader = csv.reader(f)
+    # read column headings in this csv
+    for isbn, title, author, year in reader:
+        # SQL insert into table named books 
+        db.execute( "INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)", {"isbn": isbn, "title": title, "author": author, "year": year} )
+    db.commit()
+    # close the file
+    f.close()
+
+if __name__ == "__main__":
+  main()
